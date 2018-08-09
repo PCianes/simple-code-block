ace957bcb596027872f3ac5429c75ffa75635ddf
Pablo Cianes <cianespablo@gmail.com>
2018-08-09 18:34:19 +0200

    Avoid generic name

--- core/class-simple-code-block.php
+++ core/class-simple-code-block.php
@@ -66,8 +66,8 @@ class Simple_Code_Block {
 	 * @since    1.0.0
 	 */
 	public function __construct() {
-		if ( defined( 'PLUGIN_NAME_VERSION' ) ) {
-			$this->version = PLUGIN_NAME_VERSION;
+		if ( defined( 'SIMPLE_CODE_BLOCK' ) ) {
+			$this->version = SIMPLE_CODE_BLOCK;
 		} else {
 			$this->version = '1.0.0';
 		}
